fed17a7badbebaa5401c09ae2d3d3e7ca582b1f2
heonwoochoo <cngjsdn9@gmail.com>
2023-03-10 17:02:41 +0900

    moving platform

---
+++ Source/PuzzlePlatform/Public/MovingPlatform.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORM_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMovingPlatform();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MovementSpeed = 10.f;
+};
